1d23ffb4a62e5ddd6c4fad80384f4d372426b5bf
Niginairgash <40998910+Niginairgash@users.noreply.github.com>
2025-10-22 11:54:11 +0500

    Update 05_subquery.sql

--- 1-fundamentals/SQL/05_subquery.sql
+++ 1-fundamentals/SQL/05_subquery.sql
@@ -1,1 +1,31 @@
 
+-- table: employees 
+  
+ --   | emp_id  |	name   | salary |department_id|	manager_id
+ --    -------------------------------------------------------
+ --   |  101	 |  Alice  | 50000	|    1	      |  null
+ --   |  102	 |  Bob	   | 75000	|    1	      |  101
+ --   |  103	 |  Charlie| 82000	|    2	      |  101
+ --   |  104	 |  Diana  | 45000	|    2	      |  103
+ --   |  105	 |  Eve	   | 90000	|    1	      |  101
+ --   |  106	 |  Frank  | 48000	|    3	      |  103
+
+
+-- table: departments 
+
+    -- dept_id | dept_name  | budget
+    -- ------------------------------
+    --   1	   |   IT	    | 100000
+    --   2	   |   Sales	| 80000
+    --   3	   |   HR	    | 60000
+
+
+-- Find all employees who earn more than the average salary across all employees.
+
+
+-- Find the names of all employees who work in departments that have an 'e' in their name (like 'Engineering').
+
+--  Find the names of all employees who work in departments that have a budget greater than $70,000.
+
+
+-- Find all employees who earn more than the average salary of their own department.
