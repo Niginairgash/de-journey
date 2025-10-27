f44d5492ede14bf7ab8de241d4b2009ac89c2b65
Niginairgash <40998910+Niginairgash@users.noreply.github.com>
2025-10-27 13:26:41 +0500

    Create 01-check_test.py

---
+++ 1-fundamentals/python/01-check_test.py
@@ -0,0 +1,14 @@
+# 1. Create list of first letters from words:
+words = ["apple", "banana", "cherry"]
+alphabet = [item[0] for item in words]
+
+# 2. Swap keys and values in dictionary:
+original = {"a": 1, "b": 2, "c": 3}
+org = { original[key]:key for key in original}
+# → {1: "a", 2: "b", 3: "c"}
+
+# 3. Find common elements in two lists:
+list1 = [1, 2, 3, 4]
+list2 = [3, 4, 5, 6]
+commonlist = list(set(list1) & set(list2))
+# → [3, 4]
