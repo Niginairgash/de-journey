5dd9f02063aa302efefc805406b796a471de3e83
i.nigina <i.nigina@decosystems.ru>
2025-10-29 09:36:48 +0500

    eddited test-check

--- 1-fundamentals/python/01-test-check.py
+++ 1-fundamentals/python/01-test-check.py
@@ -4,7 +4,8 @@ alphabet = [item[0] for item in words]
 
 # 2. Swap keys and values in dictionary:
 original = {"a": 1, "b": 2, "c": 3}
-org = { original[key]:key for key in original}
+#org = { original[key]:key for key in original}
+org = { value:key for key, value in original.items()}
 # → {1: "a", 2: "b", 3: "c"}
 
 # 3. Find common elements in two lists:
