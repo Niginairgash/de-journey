75cac21705ead4ae5e35b24897ec2b9ddc73fe39
Niginairgash <40998910+Niginairgash@users.noreply.github.com>
2025-10-31 14:54:54 +0500

    Update 03-csv_to_json.py

--- 1-fundamentals/python/03-csv_to_json.py
+++ 1-fundamentals/python/03-csv_to_json.py
@@ -2,12 +2,17 @@ import json
 import csv
 import pandas as pd
 
+# Using csv and json modules 
 with open('cities.csv', 'r', encoding='utf-8') as csv_file:
     data = list(csv.DictReader(csv_file, delimiter='\t'))
 
 with open('cities.json', 'w', encoding='utf-8') as json_file:
     json.dump(data, json_file, indent=2)
 
+
+# Using pandas library
 df = pd.read_csv('cities.csv', delimiter='\t', quotechar='"', escapechar='\\')
 
 df.to_json('cities.json', orient='records', lines=True, indent=2 )
+
+# !TO DO Fix improperly escaped quotes
