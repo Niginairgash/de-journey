59ce9de1c2a01a85ef27c7a9d3b7a9b2dd557e83
i.nigina <i.nigina@decosystems.ru>
2025-11-04 17:56:24 +0500

    Eddit txt file

--- 1-fundamentals/python/03-txt.py
+++ 1-fundamentals/python/03-txt.py
@@ -1,3 +1,4 @@
+import datetime
 # Python Text File Tasks
 # Beginner Tasks
 #===============================================================
@@ -5,6 +6,20 @@
 #===============================================================
 # Create a program that lets users write diary entries
 # Each entry should be timestamped and saved to "diary.txt"
+
+def simple_diary():
+    print("=== SIMPLE DIARY ===")
+    entry = input("Write your diary entry: ")
+
+    # Get current date and time
+    now = datetime.date
+    timestamp = now.strftime("%Y-%m-%d %H:%:%S")
+
+    # Save to file
+    with open('diary.txt', 'a', newline='') as file:
+        file.writelines(f"[{timestamp}] {entry}")
+
+simple_diary()
 
 
 #===============================================================
