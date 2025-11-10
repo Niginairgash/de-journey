9bd75a4c7f308a131fe0f0b7ababa8172af53357
Niginairgash <40998910+Niginairgash@users.noreply.github.com>
2025-11-10 08:30:56 +0500

    Update 01-series.py

--- 2-data-processing/pandas/01-series.py
+++ 2-data-processing/pandas/01-series.py
@@ -1,11 +1,11 @@
+import pandas as pd
 #===============================================================
 # Task 1: Create a Basic Series
 #===============================================================
-
 # Create a Series of temperatures: [25, 28, 30, 22, 26]
 # Use default index
 # Print the series
-
+temperatures = pd.Series([25, 28, 30, 22, 26])
 #===============================================================
 # Task 2: Series with Custom Index
 #===============================================================
