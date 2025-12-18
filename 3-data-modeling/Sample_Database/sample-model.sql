dc81f90851058f8769641fa0281dddb121f8b0ba
Niginairgash <40998910+Niginairgash@users.noreply.github.com>
2025-12-18 15:24:04 +0500

    Update sample-model.sql

--- 3-data-modeling/Sample_Database/sample-model.sql
+++ 3-data-modeling/Sample_Database/sample-model.sql
@@ -1,13 +1,19 @@
+
+--drop table customer;
+--drop table "order";
+--drop table orderitem ;
+--drop table product ;
+--drop table supplier ;
 /*==============================================================*/
 /* Table: Customer                                              */
 /*==============================================================*/
 create table customer (
-   customer_id                   int                 primary key,
-   firstName            varchar(40)         not null,
-   lastName             varchar(40)         not null,
-   city                 varchar(40)         null,
-   country              varchar(40)         null,
-   phone                varchar(20)         null
+   customer_id          int           primary key,
+   first_name           varchar(40)   not null,
+   last_name            varchar(40)   not null,
+   city                 varchar(40)   null,
+   country              varchar(40)   null,
+   phone                varchar(20)   null
 );
 
 
@@ -15,20 +21,20 @@ create table customer (
 /* Index: IndexCustomerName                                     */
 /*==============================================================*/
 create index IndexCustomerName on customer (
-lastName ASC,
-firstName ASC
+last_name ASC,
+first_name ASC
 );
 
 /*==============================================================*/
 /* Table: "Order"                                               */
 /*==============================================================*/
 create table "order" (
-   order_id             int                  primary key,
-   orderDate            timestamp            default current_timestamp,
-   orderNumber          varchar(10)          null,
-   customer_id          int                  not null,
-   totalAmount          decimal(12,2)        null default 0
-)
+   order_id             int            primary key,
+   order_date           timestamp      default current_timestamp,
+   order_number         varchar(10)    null,
+   customer_id          int            not null,
+   total_amount         decimal(12,2)  null default 0
+);
 
 
 /*==============================================================*/
@@ -42,17 +48,17 @@ customer_id ASC
 /* Index: IndexOrderOrderDate                                   */
 /*==============================================================*/
 create index IndexOrderOrderDate on "order" (
-orderDate ASC
+order_date ASC
 );
 
 /*==============================================================*/
 /* Table: OrderItem                                             */
 /*==============================================================*/
-create table orderItem (
-   orderItem_id         int                  primary key,
+create table order_item (
+   order_item_id        int                  primary key,
    order_id             int                  not null,
    product_id           int                  not null,
-   unitPrice            decimal(12,2)        not null default 0,
+   unit_price           decimal(12,2)        not null default 0,
    quantity             int                  not null default 1
 );
 
@@ -60,27 +66,27 @@ create table orderItem (
 /*==============================================================*/
 /* Index: IndexOrderItemOrderId                                 */
 /*==============================================================*/
-create index IndexOrderItemOrderId on orderItem (
+create index IndexOrderItemOrderId on order_item (
 order_id ASC
-)
+);
 
 /*==============================================================*/
 /* Index: IndexOrderItemProductId                               */
 /*==============================================================*/
-create index IndexOrderItemProductId on orderItem (
+create index IndexOrderItemProductId on order_item (
 product_id ASC
-)
+);
 
 /*==============================================================*/
 /* Table: Product                                               */
 /*==============================================================*/
 create table product (
    product_id           int                 primary key,
-   productName          varchar(50)         not null,
-   supplierId           int                 not null,
-   unitPrice            decimal(12,2)       null default 0,
+   product_name         varchar(50)         not null,
+   supplier_id          int                 not null,
+   unit_price           decimal(12,2)       null default 0,
    package              varchar(30)         null,
-   isDiscontinued       boolean             not null default false
+   is_discontinued      boolean             not null default false
 );
 
 
@@ -88,14 +94,14 @@ create table product (
 /* Index: IndexProductSupplierId                                */
 /*==============================================================*/
 create index IndexProductSupplierId on product (
-supplierId ASC
+supplier_id ASC
 );
 
 /*==============================================================*/
 /* Index: IndexProductName                                      */
 /*==============================================================*/
 create index IndexProductName on product (
-productName ASC
+product_name ASC
 );
 
 /*==============================================================*/
@@ -103,9 +109,9 @@ productName ASC
 /*==============================================================*/
 create table supplier (
    supplier_id          int                 primary key,
-   companyName          varchar(40)         not null,
-   contactName          varchar(50)         null,
-   contactTitle         varchar(40)         null,
+   company_name         varchar(40)         not null,
+   contact_name         varchar(50)         null,
+   contact_title        varchar(40)         null,
    city                 varchar(40)         null,
    country              varchar(40)         null,
    phone                varchar(30)         null,
