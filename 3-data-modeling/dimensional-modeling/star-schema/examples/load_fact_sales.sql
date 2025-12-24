d34deaad2524a6bc9136671c610e3f40c54635bb
Niginairgash <40998910+Niginairgash@users.noreply.github.com>
2025-12-24 17:59:26 +0500

    Create load_fact_sales.sql

---
+++ 3-data-modeling/dimensional-modeling/star-schema/examples/load_fact_sales.sql
@@ -0,0 +1,18 @@
+insert into core.fact_sales(customer_fk, product_fk, date_pk, unit_price, quantity, total_amount)
+select
+	dc.customer_pk , 
+	dp.product_pk , 
+	dd.date_pk , 
+	oi.unit_price,
+	oi.quantity, 
+	oi.unit_price * oi.quantity as total_amount
+from public."order" o 
+join public.order_item oi on o.order_id = oi.order_id 
+join core.dim_customer dc on dc.customer_id = o.customer_id 
+		and dc.is_current = true
+join core.dim_product dp on dp.product_id = oi.product_id 
+		and dp.is_current = true
+join core.dim_date dd on dd.full_date = o.order_date 
+where not exists (
+
+)
