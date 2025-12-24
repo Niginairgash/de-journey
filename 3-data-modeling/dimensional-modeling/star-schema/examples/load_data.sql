218e48601d7dee3067b7d618ac8b2930563f4b52
Niginairgash <40998910+Niginairgash@users.noreply.github.com>
2025-12-24 15:32:05 +0500

    Add date_pk column to dim_date insert statement

--- 3-data-modeling/dimensional-modeling/star-schema/examples/load_data.sql
+++ 3-data-modeling/dimensional-modeling/star-schema/examples/load_data.sql
@@ -1,17 +1,24 @@
-insert into core.dim_date (full_date, "year", "month", "day", quater)
+insert into core.dim_date (date_pk, full_date, "year", "month", "day", quater)
+with bounds as(
+	select
+		min(order_date)::date as min_dt,
+		max(order_date)::date as max_dt
+	from public."order" 
+)
 select
-	to_char(d, 'YYYYMMDD')::int 	as date_pk
+	to_char(d, 'YYYYMMDD')::int 	as date_pk,
 	d::DATE 						as full_date,
 	extract (year from d) 			as "year",
 	extract(month from d) 			as "month",
 	extract(day from d) 			as "day",
 	(extract(month from d)+2) / 3 	as quater	
 from generate_series(
-	'2000-01-01'::DATE,
-	'2030-01-01'::DATE, 
-	'1 day'::INTERVAL) as d
+	(select min_dt from bounds),
+	(select max_dt + interval '1 year' from bounds),
+	interval '1 day'
+	) as d
 -- Check if date already exists to avoid duplicates
-where not exists ( 
+where not exists (
 	select 1
 	from core.dim_date dd
 	where dd.full_date = d::date
