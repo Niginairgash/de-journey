1871e22195c7ef17daf9e7a92735fee858badee8
Niginairgash <40998910+Niginairgash@users.noreply.github.com>
2025-12-25 10:55:19 +0500

    Update e-commerce.sql

--- 3-data-modeling/dimensional-modeling/star-schema/examples/e-commerce.sql
+++ 3-data-modeling/dimensional-modeling/star-schema/examples/e-commerce.sql
@@ -1,6 +1,6 @@
 --create schema core;
 
-create table dim_customer(
+create table core.dim_customer(
 	customer_pk 	serial 		primary key,
 	customer_id 	int 		not null,
 	first_name		varchar(40)	not null,
@@ -13,7 +13,7 @@ create table dim_customer(
 	is_current		boolean		not null default true
 );
 
-create table dim_product(
+create table core.dim_product(
 	product_pk 				serial		primary key,
 	product_id				int4		not null,
 	product_name			varchar(50)	not null,
@@ -21,12 +21,13 @@ create table dim_product(
 	suppliers_contact_name	varchar(50)	not null,
 	unit_price				numeric(12,2) not null,
 	package					varchar(30)	not null,
-	start_dt				timestamp		not null,
+	is_discontinued			boolean 	not null,
+	start_dt				timestamp	not null,
 	end_dt					timestamp,
 	is_current				boolean		not null default true	
 );
 
-create table dim_date(
+create table core.dim_date(
 	date_pk 		int			primary key,
 	full_date		date		not null,
 	year			int			not null,
@@ -35,11 +36,11 @@ create table dim_date(
 	quater			int   		not null	
 );
 
-create table fact_sales(
+create table core.fact_sales(
 	sales_pk 		serial		primary key,
-	customer_fk		int 		not null references  dim_customer(customer_pk),
-	product_fk		int			not null references dim_product(product_pk),
-	date_pk			int 		not null references dim_date(date_pk),
+	customer_fk		int 		not null references 	core.dim_customer(customer_pk),
+	product_fk		int			not null references 	core.dim_product(product_pk),
+	date_pk			int 		not null references 	core.dim_date(date_pk),
 	unit_price		numeric(12, 2)	not null,
 	quantity		int4		not null,
 	total_amount	numeric(12, 2) not null
