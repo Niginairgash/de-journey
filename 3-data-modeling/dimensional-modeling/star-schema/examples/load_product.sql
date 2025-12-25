56c2b4927aa52437ec60b688f7b7a62257404c5b
Niginairgash <40998910+Niginairgash@users.noreply.github.com>
2025-12-25 11:04:09 +0500

    Update load_product.sql

--- 3-data-modeling/dimensional-modeling/star-schema/examples/load_product.sql
+++ 3-data-modeling/dimensional-modeling/star-schema/examples/load_product.sql
@@ -6,8 +6,8 @@ change as (
 	select 
 		p.product_id ,
 		p.product_name ,
-		s.suppliers_company_name ,
-		s.suppliers_contact_name ,
+		s.company_name ,
+		s.contact_name ,
 		p.unit_price ,
 		p.package,
     p.is_discontinued
@@ -16,31 +16,41 @@ change as (
 	join core.dim_product dp on dp.product_id = p.product_id
 		and dp.is_current = true
 	where (
-			dp.product_name             is distinct from p.product_name
-  or dp.suppliers_company_name    is distinct from s.suppliers_company_name
-  or dp.suppliers_contact_name    is distinct from s.suppliers_contact_name
+			dp.product_name       is distinct from p.product_name
+  or dp.suppliers_company_name    is distinct from s.company_name
+  or dp.suppliers_contact_name    is distinct from s.contact_name
   or dp.unit_price                is distinct from p.unit_price
   or dp.package                   is distinct from p.package
   or dp.is_discontinued           is distinct from p.is_discontinued
 	)
 )
-update core.dim_product dp
-	set end_dt = b.batch_tm,
-		is_current = false
-from change c, batch b
-where dp.product_id = c.product_id 
-	and is_current = true;
+--update core.dim_product dp
+--	set end_dt = b.batch_tm,
+--		is_current = false
+--from change c, batch b
+--where dp.product_id = c.product_id 
+--	and is_current = true;
 
 insert into core.dim_product (product_id,	product_name, suppliers_company_name, suppliers_contact_name, unit_price, package, is_discontinued, start_dt)
 select 
-	product_id,	product_name, suppliers_company_name, suppliers_contact_name, unit_price, package, is_discontinued,
+	product_id,	
+	product_name, 
+	company_name, 
+	contact_name, 
+	unit_price, package, 
+	is_discontinued,
 	b.batch_tm as start_dt 
 from
 (
 	select
-		product_id,	product_name, suppliers_company_name, suppliers_contact_name, unit_price, package, is_discontinued
+		product_id,	
+		product_name, 
+		company_name, 
+		contact_name, 
+		unit_price, package, 
+		is_discontinued
 	from public.product p 
-  join supplier s on s.supplier_id = p.supplier_id
+  	join supplier s on s.supplier_id = p.supplier_id
 	where not exists(  
 		select 1
 		from core.dim_product dp
@@ -50,9 +60,8 @@ from
 	union all
 
 	select
-		product_id,	product_name, suppliers_company_name, suppliers_contact_name, unit_price, package, is_discontinued
+		product_id,	product_name, company_name, contact_name, unit_price, package, is_discontinued
 	from change
 	
 ) src
 cross join batch b
-
